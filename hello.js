62c6926e012e63867e8e77bfa9e6e40315ce5917
Keerti Toppo <keertitoppo48@gmail.com>
2024-03-11 15:33:29 +0530

    Updated package.json, procfile, and hello.js PORT

--- hello.js
+++ hello.js
@@ -96,6 +96,7 @@ console.log("Server Listening on port 8080...");
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function(req, res) {
     var q = url.parse(req.url, true);
@@ -114,6 +115,6 @@ http.createServer(function(req, res) {
         res.write(data);
         return res.end();
         }) 
-}).listen(8080);
+}).listen(PORT);
 
 console.log("Server Listening on port 8080...");
